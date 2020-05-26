f36b7d568406987a7d1435e9627dc55073049a78
repl.it user <replituser@example.com>
2020-05-26 00:22:47 +0000

    carga base

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    
+    
+    //Teste teste = new Teste();
+    //teste.executar();
+
+ 
+
+    System.out.println("teste ok, fim!");
+  }
+}
